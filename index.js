616a25a9a1ae7e713145a2105d7f6d84a97bdcc6
Benjamin Christau <benjamin.christau@oyez.fr>
2020-02-13 18:18:10 +0100

    Add the an option to get all the environment variables from the '.env' file with the default import

--- index.js
+++ index.js
@@ -1,64 +1,106 @@
-const {readFileSync} = require('fs')
-const dotenv = require('dotenv')
+const { readFileSync } = require("fs");
+const dotenv = require("dotenv");
 
-module.exports = ({types: t}) => ({
-  name: 'dotenv-import',
+module.exports = ({ types: t }) => ({
+  name: "dotenv-import",
 
   pre() {
-    this.opts = Object.assign({
-      moduleName: '@env',
-      path: '.env',
-      whitelist: null,
-      blacklist: null,
-      safe: false,
-      allowUndefined: false
-    }, this.opts)
+    this.opts = Object.assign(
+      {
+        moduleName: "@env",
+        path: ".env",
+        whitelist: null,
+        blacklist: null,
+        safe: false,
+        allowUndefined: false,
+        defaultImportName: "allEnv"
+      },
+      this.opts
+    );
 
     if (this.opts.safe) {
-      this.env = dotenv.parse(readFileSync(this.opts.path))
+      this.env = dotenv.parse(readFileSync(this.opts.path));
     } else {
       dotenv.config({
         path: this.opts.path
-      })
-      this.env = process.env
+      });
+      this.env = process.env;
     }
   },
 
   visitor: {
-    ImportDeclaration(path, {opts}) {
+    ImportDeclaration(path, { opts }) {
       if (path.node.source.value === opts.moduleName) {
         path.node.specifiers.forEach((specifier, idx) => {
-          if (specifier.type === 'ImportDefaultSpecifier') {
-            throw path.get('specifiers')[idx].buildCodeFrameError('Default import is not supported')
+          if (specifier.type === "ImportDefaultSpecifier") {
+            if (this.opts.defaultImportName) {
+              const defaultImportBinding = path.scope.getBinding(
+                this.opts.defaultImportName
+              );
+              const defaultImportPaths =
+                defaultImportBinding && defaultImportBinding.referencePaths
+                  ? defaultImportBinding.referencePaths
+                  : null;
+              if (defaultImportPaths) {
+                defaultImportPaths.forEach(refPath =>
+                  refPath.replaceWith(t.valueToNode(this.env))
+                );
+              }
+            } else {
+              throw path
+                .get("specifiers")
+                [idx].buildCodeFrameError("Default import is not supported");
+            }
+          } else {
+            if (specifier.type === "ImportNamespaceSpecifier") {
+              throw path
+                .get("specifiers")
+                [idx].buildCodeFrameError("Wildcard import is not supported");
+            }
+
+            const importedId = specifier.imported.name;
+            const localId = specifier.local.name;
+
+            if (
+              Array.isArray(opts.whitelist) &&
+              !opts.whitelist.includes(importedId)
+            ) {
+              throw path
+                .get("specifiers")
+                [idx].buildCodeFrameError(
+                  `"${importedId}" was not whitelisted`
+                );
+            }
+
+            if (
+              Array.isArray(opts.blacklist) &&
+              opts.blacklist.includes(importedId)
+            ) {
+              throw path
+                .get("specifiers")
+                [idx].buildCodeFrameError(`"${importedId}" was blacklisted`);
+            }
+
+            if (
+              !opts.allowUndefined &&
+              !Object.prototype.hasOwnProperty.call(this.env, importedId)
+            ) {
+              throw path
+                .get("specifiers")
+                [idx].buildCodeFrameError(
+                  `"${importedId}" is not defined in ${opts.path}`
+                );
+            }
+
+            const binding = path.scope.getBinding(localId);
+            binding.referencePaths.forEach(refPath => {
+              refPath.replaceWith(t.valueToNode(this.env[importedId]));
+            });
           }
+        });
 
-          if (specifier.type === 'ImportNamespaceSpecifier') {
-            throw path.get('specifiers')[idx].buildCodeFrameError('Wildcard import is not supported')
-          }
-
-          const importedId = specifier.imported.name
-          const localId = specifier.local.name
-
-          if (Array.isArray(opts.whitelist) && !opts.whitelist.includes(importedId)) {
-            throw path.get('specifiers')[idx].buildCodeFrameError(`"${importedId}" was not whitelisted`)
-          }
-
-          if (Array.isArray(opts.blacklist) && opts.blacklist.includes(importedId)) {
-            throw path.get('specifiers')[idx].buildCodeFrameError(`"${importedId}" was blacklisted`)
-          }
-
-          if (!opts.allowUndefined && !Object.prototype.hasOwnProperty.call(this.env, importedId)) {
-            throw path.get('specifiers')[idx].buildCodeFrameError(`"${importedId}" is not defined in ${opts.path}`)
-          }
-
-          const binding = path.scope.getBinding(localId)
-          binding.referencePaths.forEach(refPath => {
-            refPath.replaceWith(t.valueToNode(this.env[importedId]))
-          })
-        })
-
-        path.remove()
+        path.remove();
       }
     }
   }
-})
+});
